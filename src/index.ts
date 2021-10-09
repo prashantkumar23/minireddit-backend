a7af8fc4035710192a5046348f8580dbc8a56df8
Prashant Kumar <prashantdrew23@gmail.com>
2021-10-09 14:09:07 +0530

    inital deploy

--- src/index.ts
+++ src/index.ts
@@ -65,7 +65,7 @@ const main = async () => {
         httpOnly: true,
         sameSite: "lax", // csrf
         secure: __prod__, // cookie only works in https
-        domain: __prod__ ? "minireddit.vercel.app" : undefined,
+        domain: __prod__ ? process.env.CORS_ORIGIN : undefined,
       },
       saveUninitialized: false,
       secret: process.env.SESSION_SECRET as string,
